353967e0311f7298ecbd9cda0dfaeaf2bf18f2bc
Victoria-20 <victoriaocon111@gmail.com>
2023-02-12 13:35:57 +0300

    Fix issue

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,12 +2,12 @@
 """ Module for logging messages"""
 import logging
 import re
+from typing import List
 
 
-def filter_datum(fields: list[str], redaction: str,
+def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    """ Returns the log message obfuscated
-        uses re.sub to perform the substitution with a single regex."""
+    """ Returns the log message obfuscated """
     holder = message
     for field in fields:
         holder = re.sub(field + "=.*?" + separator,
