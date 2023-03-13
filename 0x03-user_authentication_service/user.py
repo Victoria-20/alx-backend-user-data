68aa1bdaca75e66f87c679ebc08bb7ebef85eb7d
Victoria-20 <victoriaocon111@gmail.com>
2023-03-13 12:33:44 +0300

    0. User model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+""" SQLAlchemy model"""
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """ Model user connecting to database `user`"""
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
